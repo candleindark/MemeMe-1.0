6b3236fcfafac410dfb1a0b8b52fff4d954d1228
Isaac To <isaac.chun.to@gmail.com>
2016-07-16 22:47:19 -0500

    Overriding viewWillAppear

--- MemeMe 1.0/ViewController.swift
+++ MemeMe 1.0/ViewController.swift
@@ -10,16 +10,16 @@ import UIKit
 
 class MemeEditorController: UIViewController {
 
+    // MARK: UIViewController methods
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
     }
-
-
+    
+    
 }
 
